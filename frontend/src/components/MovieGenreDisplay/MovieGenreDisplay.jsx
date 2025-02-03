e1f9ab5c7e939dba0378eda04a50d455c49e9788
na1969na <nakmtn07@gmail.com>
2025-02-02 23:29:37 -0800

    add usequery

--- frontend/src/components/MovieGenreDisplay/MovieGenreDisplay.jsx
+++ frontend/src/components/MovieGenreDisplay/MovieGenreDisplay.jsx
@@ -27,51 +27,52 @@ const MovieGenreDisplay = () => {
 
   console.log(topRatedMovies);
 
-  // if (isLoading) return <p>Loading...</p>;
-  // if (error) return <p>Error fetching genres: {error.message}</p>;
-
   return (
     <div className="py-12 font-dmsans">
       <h1 className="text-xl sm:text-5xl md:text-5xl pb-5 font-bold text-center">
         Choose Your Genre
       </h1>
-      <p className="text-lg text-center">
-        Pair your favorite movie genres with the perfect recipes!
+      <p className="text-lg text-center mb-8">
+        Discover top-rated movies by genre and find your next favorite film.
+        Click on a genre to explore more.
       </p>
-      <p className="text-lg text-center mb-10">
-        Explore films and enjoy a matching dish to elevate your movie night
-        experience.
-      </p>
-      <div className="relative overflow-hidden pb-10">
-        {topRatedMovies &&
-          Object.entries(topRatedMovies).map(([genre, movie]) => (
-            <div key={movie.id}>
-              <Link
-                className="flex text-center items-center py-5 text-3xl rounded-sm hover:opacity-75 transition duration-300 cursor-pointer ease-in-out gap-5"
-                to={{
-                  pathname: "/movie-recipe",
-                }}
-                state={{
-                  genreId: genre.id,
-                  genreName: genre.name,
-                }}
-              >
-                <h2 className="text-4xl font-semibold">{genre.name}</h2>
-                <ChevronRightIcon className="h-8 w-8" />
-              </Link>
+      <div className="">
+        {genres.map((genre) => (
+          <div key={genre.id}>
+            <Link
+              className="flex text-center items-center p-5 text-3xl rounded-sm hover:opacity-75 transition duration-300 cursor-pointer ease-in-out gap-5"
+              to={{
+                pathname: "/movie-recipe",
+              }}
+              state={{
+                genreId: genre.id,
+                genreName: genre.name,
+              }}
+            >
+              <h2 className="text-4xl font-semibold">{genre.name}</h2>
+              <ChevronRightIcon className="h-8 w-8" />
+            </Link>
+            <div className="relative overflow-hidden pb-10">
               <div className="flex gap-3 overflow-x-auto scrollbar-hide scroll-smooth">
-                <div className="min-w-[200px] lg:min-w-[250px] transition-transform duration-300 ease-in-out group">
-                  <div className="w-full">
-                    <img
-                      src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                      alt={movie.title}
-                      className="w-full h-auto rounded-3xl"
-                    />
-                  </div>
-                </div>
+                {topRatedMovies &&
+                  topRatedMovies[genre.name]?.map((movie) => (
+                    <div
+                      key={movie.id}
+                      className="min-w-[200px] lg:min-w-[250px] transition-transform duration-300 ease-in-out group"
+                    >
+                      <div className="w-full">
+                        <img
+                          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                          alt={movie.title}
+                          className="w-full h-auto rounded-3xl"
+                        />
+                      </div>
+                    </div>
+                  ))}
               </div>
             </div>
-          ))}
+          </div>
+        ))}
       </div>
     </div>
   );
