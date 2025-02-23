66f19e1d3e1d80a45d91c4704d16ba8d2684838c
Nana Okamoto <nakmtn07@gmail.com>
2025-02-22 16:23:37 -0800

    fix search results design

--- frontend/src/pages/SearchResults/SearchResults.jsx
+++ frontend/src/pages/SearchResults/SearchResults.jsx
@@ -38,38 +38,40 @@ const SearchResults = () => {
   };
 
   return (
-    <div className="bg-primaryColor">
-      <div className="flex px-20 py-8 bg-quaternaryColor">
-        <div className="border-b border-black flex w-full justify-between">
+    <div>
+      <div className="flex px-20 pt-20 pb-10 bg-quaternaryColor">
+        <div className="border-b border-black flex w-full">
           <input
             type="text"
             placeholder="Search movies..."
             value={inputValue}
             onChange={handleInputChange}
             onKeyDown={handleInputKeyDown}
-            className="px-5 pb-2 outline-none bg-transparent text-5xl placeholder-black"
+            className="w-full px-3 outline-none bg-transparent text-3xl placeholder-black"
           />
           <button className="ml-2 p-2 flex items-center" onClick={handleSearch}>
-            <ArrowRightIcon className="h-12 w-12 mr-1" />
+            <ArrowRightIcon className="h-10 w-10 mr-1" />
           </button>
         </div>
       </div>
-      <div className="px-28 py-10 text-tertiaryColor">
+      <div className="px-20 py-10">
         {data && data.length === 0 ? (
-          <p className="font-bold text-3xl text-center">No movies found</p>
+          <p className="font-semibold text-3xl text-center text-tertiaryColor">
+            No movies found
+          </p>
         ) : (
-          <p className="font-bold text-2xl mb-10">
-            movies found by {searchQuery}
+          <p className="font-semibold text-3xl mb-10 text-tertiaryColor">
+            movies found by &quot;{searchQuery}&quot;
           </p>
         )}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-5">
           {data &&
             data.map(
               (movie) =>
                 movie.poster_path && (
                   <div
                     key={movie.id}
-                    className="transition-transform duration-300 ease-in-out group hover:text-quaternaryColor"
+                    className="transition-transform duration-300 ease-in-out group"
                   >
                     <Link
                       to={{
@@ -82,12 +84,10 @@ const SearchResults = () => {
                         alt={movie.title}
                         className="w-full h-auto hover:opacity-60 transition duration-300"
                       />
-                      <div className="px-3 pt-10 text-center">
-                        <h1 className="text-3xl">
-                          {movie.title}
-                        </h1>
-                        <p className="text-lg">
-                          {movie.release_date}
+                      <div className="px-3 pt-10">
+                        <h1 className="text-xl font-semibold">{movie.title}</h1>
+                        <p className="text-lg pt-2 pb-5">
+                          {new Date(movie.release_date).getFullYear()}
                         </p>
                       </div>
                     </Link>
@@ -95,8 +95,8 @@ const SearchResults = () => {
                 )
             )}
 
-          {isLoading && <div>Carregando mais dados...</div>}
-          {isError && <div>Carregando mais dados...</div>}
+          {isLoading && <div>Loading data...</div>}
+          {isError && <div>Error loading data...</div>}
         </div>
       </div>
     </div>
